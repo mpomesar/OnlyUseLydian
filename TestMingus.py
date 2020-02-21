7e77ded5f0f320244aa5225c7865891cc8ca9d01
Marti Pomes <marti.pomes@omya.com>
2020-02-21 23:23:07 +0100

    MAJOR - Initial Commit

---
+++ TestMingus.py
@@ -0,0 +1,10 @@
+'''
+This is an icredibly bad attempt to create music.
+
+Since we know it's not going to work lets try to make a fool of ourselfs as much as we can
+
+In this first document, lets see how NOT to use mingus library in Python
+'''
+
+
+
